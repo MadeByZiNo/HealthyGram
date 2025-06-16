0138740b3995b4a77046e0934294e40e560a52f4
MadeByZino <jh990517@naver.com>
2025-06-16 17:26:47 +0900

    [#11] feat : 피드jpa레포지토리 추가
    ---
    
    - FeedJpaRepository.java 생성

---
+++ src/main/java/madebyzino/HealthyGram/domain/feed/FeedJpaRepository.java
@@ -0,0 +1,8 @@
+package madebyzino.HealthyGram.domain.feed;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FeedJpaRepository extends JpaRepository<Feed, Long> {
+
+
+}
